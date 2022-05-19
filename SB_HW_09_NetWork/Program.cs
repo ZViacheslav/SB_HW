3cadb69dad24d21c88fbdaf3ca5bddf5ddb6c8dc
TB <2061704@mail.ru>
2022-05-19 14:37:23 +0300

    Add method FileSend

--- SB_HW_09_NetWork/Program.cs
+++ SB_HW_09_NetWork/Program.cs
@@ -11,6 +11,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Example_941
 {
@@ -41,7 +45,7 @@ namespace Example_941
 
             #endregion
 
-            string token = File.ReadAllText(@"D:\token.txt");
+            string token = "5305716537:AAEXs1BGZFsO1f0yKAbr1rfIgLQPJkZWOl0";
 
             #region WebClient
             //WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
@@ -109,37 +113,94 @@ namespace Example_941
 
             bot = new TelegramBotClient(token);
 
-            bot.OnMessage += MessageListener;
+            //bot.OnMessage += MessageListener;
+
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
 
             #endregion
 
         }
 
-        private static void MessageListener(object sender, MessageEventArgs e)
+        #region NewMethods
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-
-            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
-
-
-            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            // Некоторые действия
+            //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            //if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
-                Console.WriteLine(e.Message.Document.FileId);
-                Console.WriteLine(e.Message.Document.FileName);
-                Console.WriteLine(e.Message.Document.FileSize);
-
-                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+                var message = update.Message;
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+                    return;
+                }
+                if (message.Text.ToLower() == "wago")
+                {
+                    FileSend("_Ваго.txt", "", update);
+                }
             }
 
-            if (e.Message.Text == null) return;
+            if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                Console.WriteLine(new String('-', 50));
+                Console.WriteLine($"     FileId: {update.Message.Document.FileId}");
+                Console.WriteLine($"   FileName: {update.Message.Document.FileName}");
+                Console.WriteLine($"   FileSize: {update.Message.Document.FileSize} байт");
 
-            var messageText = e.Message.Text;
-
-
-            bot.SendTextMessageAsync(e.Message.Chat.Id,
-                $"{messageText}"
-                );
+                DownLoad(update.Message.Document.FileId, update.Message.Document.FileName);
+            }
         }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        #endregion
+
+        #region OldMethods      
+        //private static void MessageListener(object sender, MessageEventArgs e)
+        //{
+        //    string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+        //    Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
+
+
+        //    if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+        //    {
+        //        Console.WriteLine(e.Message.Document.FileId);
+        //        Console.WriteLine(e.Message.Document.FileName);
+        //        Console.WriteLine(e.Message.Document.FileSize);
+
+        //        DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+        //    }
+
+        //    if (e.Message.Text == null) return;
+
+        //    var messageText = e.Message.Text;
+
+
+        //    bot.SendTextMessageAsync(e.Message.Chat.Id,
+        //        $"{messageText}"
+        //        );
+        //}
 
         static async void DownLoad(string fileId, string path)
         {
@@ -150,6 +211,35 @@ namespace Example_941
 
             fs.Dispose();
         }
+
+        static async void FileSend(string fileId, string path, Update update)
+        {
+            using (var stream = System.IO.File.OpenRead(path+fileId))
+            {
+                InputOnlineFile iof = new InputOnlineFile(stream); //оставляем также 
+                iof.FileName = fileId;
+                var send = await bot.SendDocumentAsync(update.Message.Chat.Id, iof, "Получи текстовой файл!");
+            }
+        }
+
+        //public Task<Message> SendDocumentAsync(ChatId chatId, InputOnlineFile imageFile, string caption = "",
+        //    bool disableNotification = false,
+        //    int replyToMessageId = 0,
+        //    IReplyMarkup replyMarkup = null,
+        //    CancellationToken cancellationToken = default)
+        //{
+        //    var additionalParameters = new Dictionary<string, object>
+        //    {
+        //        {"caption", caption}
+        //    };
+
+        //    return SendMessageAsync(MessageType.DocumentMessage, chatId, document, disableNotification,
+        //        replyToMessageId,
+        //        replyMarkup, additionalParameters, cancellationToken);
+        //}
+        #endregion
+
+
     }
 }
 
